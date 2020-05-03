0d2011aaa5d9b0ef24e4e891f6e85fd71c6c8cb3
Renan Abbade <renanabbade1997@gmail.com>
2020-05-03 17:44:03 -0300

    Commit inicial

---
+++ src/main/java/Com/Renan/Spring/CursoSpringApplication.java
@@ -0,0 +1,13 @@
+package Com.Renan.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/Renan/Spring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.Renan.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

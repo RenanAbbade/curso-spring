41a33b902673276688cb6c97e7b6c85ed3c07822
Renan Abbade <renanabbade1997@gmail.com>
2020-05-03 21:03:34 -0300

    Criação da base de dados via ORM JPA no H2 DB

--- src/main/java/Com/Renan/Spring/domain/Categoria.java
+++ src/main/java/Com/Renan/Spring/domain/Categoria.java
@@ -2,13 +2,20 @@ package Com.Renan.Spring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable { /**Implements Serializable para que os objs sejam convertidos para bytes para trafegar em redes e etc.
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;

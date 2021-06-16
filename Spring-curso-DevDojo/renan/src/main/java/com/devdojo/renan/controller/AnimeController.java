668331c62498674b2b158d7267f8ef8ea462b658
Renan Abbade <renanabbade1997@gmail.com>
2021-06-16 17:45:02 -0300

    Método gedById & tratamento inicial da exception via yml

--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/controller/AnimeController.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/controller/AnimeController.java
@@ -6,7 +6,10 @@ import com.devdojo.renan.service.AnimeService;
 import com.devdojo.renan.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +28,14 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return animeService.listaAll();
+        return ResponseEntity.ok(animeService.listaAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findByUId(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
 
--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/service/AnimeService.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/service/AnimeService.java
@@ -1,14 +1,25 @@
 package com.devdojo.renan.service;
 
 import com.devdojo.renan.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
-    public List<Anime> listaAll(){
-        return List.of(new Anime(1L, "Naruto"), new Anime(2L, "Berserk"));
+    private List<Anime> animes = List.of(new Anime(1L, "Naruto"), new Anime(2L, "Berserk"));
+
+    public List<Anime> listaAll() {
+        return animes;
     }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime  not found"));
+    }//Se não for achado o id passado no path, retorna-se uma badRequest
 }

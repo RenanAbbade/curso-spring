62b759b15f727153885fd19163e155ff03555968
Renan Abbade <renanabbade1997@gmail.com>
2021-06-23 19:42:49 -0300

    Spring Data JPA Test: update, delete and FindBy

--- Spring-curso-DevDojo/renan/src/test/java/com/devdojo/renan/repository/AnimeRepositoryTest.java
+++ Spring-curso-DevDojo/renan/src/test/java/com/devdojo/renan/repository/AnimeRepositoryTest.java
@@ -1,15 +1,20 @@
 package com.devdojo.renan.repository;
 
 import com.devdojo.renan.domain.Anime;
+import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @DataJpaTest
 @DisplayName("Tests for anime repository")
+@Log4j2
 class AnimeRepositoryTest {
 
     @Autowired
@@ -26,16 +31,59 @@ class AnimeRepositoryTest {
         Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());//Verifico se o name do anime a ser salvo é o mesmo que solicitei.
     }
 
-//    @Test
-//    @DisplayName("Saves update`s anime when successful")
-//    void save_PersistAnime_WhenSuccessful(){//Pattern de nomeação do metódo de testes: nomeMetodo_OqueDeveFazer_ResultadoEsperado
-//        Anime animeToBeSaved = createdAnime();
-//        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
-//
-//        Assertions.assertThat(animeSaved).isNotNull(); //org.assertj
-//        Assertions.assertThat(animeSaved.getId()).isNotNull(); //Verifico se o id foi automaticamente gerado como esperado
-//        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());//Verifico se o name do anime a ser salvo é o mesmo que solicitei.
-//    }
+    @Test
+    @DisplayName("Saves update`s anime when successful")
+    void save_UpdateAnime_WhenSuccessful(){//Pattern de nomeação do metódo de testes: nomeMetodo_OqueDeveFazer_ResultadoEsperado
+        Anime animeToBeSaved = createdAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        //update flux
+        animeSaved.setName("Mushouku Tensei");
+        Anime animeUpdated = this.animeRepository.save(animeSaved);
+
+        Assertions.assertThat(animeUpdated).isNotNull(); //org.assertj
+        Assertions.assertThat(animeUpdated.getId()).isNotNull(); //Verifico se o id foi automaticamente gerado como esperado
+        Assertions.assertThat(animeUpdated.getName()).isEqualToIgnoringCase("Mushouku Tensei");//Verifico se o name do anime a ser salvo é o mesmo que solicitei.
+    }
+
+    @Test
+    @DisplayName("Delete removes anime when successful")
+    void delete_RemovesAnime_WhenSuccessful(){//Pattern de nomeação do metódo de testes: nomeMetodo_OqueDeveFazer_ResultadoEsperado
+        Anime animeToBeSaved = createdAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+       //delete flux
+        this.animeRepository.delete(animeSaved);
+
+        //Como o método de delete retorna Void, para validar se o argumento foi deltado, deve-se busca-lo por id.
+        Optional<Anime> animeOptional = this.animeRepository.findById(animeSaved.getId());
+
+        Assertions.assertThat(animeOptional).isEmpty();
+
+    }
+
+    @Test
+    @DisplayName("Find by name, returns a List of anime when successful")
+    void findByName_ReturnsListAnime_WhenSuccessful(){//Pattern de nomeação do metódo de testes: nomeMetodo_OqueDeveFazer_ResultadoEsperado
+        Anime animeToBeSaved = createdAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        List<Anime> animes = this.animeRepository.findByName(animeSaved.getName());
+
+        Assertions.assertThat(animes).isNotEmpty();
+        Assertions.assertThat(animes).contains(animeSaved);
+
+
+    }
+
+
+    @Test
+    @DisplayName("Find by name, returns a empty List  when no object is found")
+    void findByName_ReturnsEmptyList_WhenAnimeNotFound(){//Pattern de nomeação do metódo de testes: nomeMetodo_OqueDeveFazer_ResultadoEsperado
+        List<Anime> animes = this.animeRepository.findByName("xxx");
+
+        Assertions.assertThat(animes).isEmpty();
+    }
 
     private Anime createdAnime(){
         return Anime.builder().name("Naruto Shippuden").build();

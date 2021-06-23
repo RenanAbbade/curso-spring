5c7ba0b10b400e2e6815dd86ad58af50317ae2e8
Renan Abbade <renanabbade1997@gmail.com>
2021-06-22 23:29:34 -0300

    RestTemplate: Post and Post Exchange

--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/client/SpringClient.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/client/SpringClient.java
@@ -1,10 +1,10 @@
 package com.devdojo.renan.client;
 
 import com.devdojo.renan.domain.Anime;
+import com.devdojo.renan.dto.AnimePostDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -16,6 +16,7 @@ public class SpringClient {
         // e pode mapear o objeto via jackson de acordo com o
         // .class passado no segundo parametro
         // o id está sendo passado como 1, no terceiro parametro
+        /*
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/api/v1/animes/1", Anime.class);
         log.info(entity);
 
@@ -30,6 +31,17 @@ public class SpringClient {
         }); //Utilizando Array
 
         log.info(animesList.getBody());
+*/
+        //POST
+        Anime anime = Anime.builder().name("Kingdom").build();
 
+        //Anime animeResponse = new RestTemplate().postForObject("http://localhost:8080/api/v1/animes/",anime, Anime.class); //URL, Objeto request, objeto Response.class
+
+        //POST Exchange -> Posso enviar headers http no exchange
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<Anime> animeResponse = new RestTemplate().exchange("http://localhost:8080/api/v1/animes/", HttpMethod.POST, new HttpEntity<>(anime, httpHeaders), Anime.class); //URL, Objeto request, objeto Response.class
+
+        log.info(animeResponse);
     }
 }
--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/dto/AnimePostDTO.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/dto/AnimePostDTO.java
@@ -1,15 +1,20 @@
 package com.devdojo.renan.dto;
 
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostDTO { //DTOClass
     //Validando campos obrigatórios com spring-boot-starter-validation
     @NotEmpty(message = "The name of the anime cannot be empty")
     @NotNull(message = "The name of the anime cannot be null")
+    @JsonProperty("name")
     private String name;
 }

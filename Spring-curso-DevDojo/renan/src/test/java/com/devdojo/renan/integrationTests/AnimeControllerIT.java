72f3dd099c45535087ec86ac58a847f9de15c734
Renan Abbade <renanabbade1997@gmail.com>
2021-06-26 23:45:52 -0300

    Testes de Integração pt 1

---
+++ Spring-curso-DevDojo/renan/src/test/java/com/devdojo/renan/integrationTests/AnimeControllerIT.java
@@ -0,0 +1,51 @@
+package com.devdojo.renan.integrationTests;
+
+import com.devdojo.renan.domain.Anime;
+import com.devdojo.renan.repository.AnimeRepository;
+import com.devdojo.renan.util.AnimeCreator;
+import com.devdojo.renan.wrapper.PageableResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+class AnimeControllerIT {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private AnimeRepository animeRepository;
+
+
+    @Test
+    @DisplayName("List returns list of anime inside page object when successful")
+    void list_ReturnsListOfAnimeInsidePageObject_WhenSuccessful() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createAnimeToBeSaved());
+
+        String expectedName = savedAnime.getName();
+
+        PageableResponse<Anime> animePage = testRestTemplate.exchange("/api/v1/animes", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animePage).isNotNull();
+
+        Assertions.assertThat(animePage.toList()).isNotEmpty().hasSize(1);
+
+        Assertions.assertThat(animePage.toList().get(0).getName()).isEqualTo(expectedName);
+
+
+    }
+
+}

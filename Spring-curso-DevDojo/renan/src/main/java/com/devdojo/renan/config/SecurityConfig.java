3f9c4f1afc63d21051afd1957ad4cdbe98a560b8
Renan Abbade <renanabbade1997@gmail.com>
2021-07-20 22:48:43 -0300

    Spring Security pt 01 - Autenticação em memória

---
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.devdojo.renan.config;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//Configuração que irá ser automaticamente carregada pelos filtros com a annotation abaixo
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Neste método será configurado o que queremos proteger com o protocolo http
+    //
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Quando se receber uma requisição http, digo que todas elas deverão passar pela autorização
+        //Todas as requests que baterem no controller deverão passar por esta autenticação básica
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    //O método abaixo terá como objetivo a criação de um usuário em memória
+    //É necessário definir uma criptografia para a senha do usuário em memória
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", passwordEncoder.encode("test"));
+        auth.inMemoryAuthentication()
+                .withUser("renan")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
+}

6f51b0bf46e54b29f79d3d29a4c4529ce662955b
Renan Abbade <renanabbade1997@gmail.com>
2021-07-23 10:45:55 -0300

    Spring Security pt 02 - Segurança a nível de métodos com PreAuthori

--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/config/SecurityConfig.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.devdojo.renan.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 //Configuração que irá ser automaticamente carregada pelos filtros com a annotation abaixo
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)//Quando incluida esta annotation, é gerado um password automatico, também habilitando os recursos de Spring Security por método
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     //Neste método será configurado o que queremos proteger com o protocolo http
@@ -23,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //Todas as requests que baterem no controller deverão passar por esta autenticação básica
         //CSRF Token: Utilizado para impedir que outro usuário execute ações em nome do user anteriormente logado
         //Medida de segurança utilizada de modo a invalidar tentativas de passagem de identidade.
+
+        //O cross-site request forgery(csrf), em português falsificação de solicitação entre sites, também conhecido como ataque de um clique ou montagem de sessão, é um tipo de exploit malicioso de um website, no qual comandos não autorizados são transmitidos a partir de um usuário em quem a aplicação web confia.
         http.csrf().disable()
                 //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // O withHttpOnlyFalse é para prover o cookie a aplicação frontend
                 //.and()
--- Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/controller/AnimeController.java
+++ Spring-curso-DevDojo/renan/src/main/java/com/devdojo/renan/controller/AnimeController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -47,7 +48,11 @@ public class AnimeController {
         return ResponseEntity.ok(animeService.findByName(name));
     }
 
+    //Como o método save pode afetar a integridade do bd da aplicação
+    //Se faz necessário a utilização de segurança a nivel de método por role
+    //@PreAuthorize(role)
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostDTO animePostDTO){ //Inserindo o @Valid, indicando ao Spring que este objeto contem validações em seus campos
         return new ResponseEntity(animeService.save(animePostDTO), HttpStatus.CREATED);
     }
